1c7fc4c4a72b41a91ca1742d0f3c4bb31e5207cf
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-12-28 22:08:32 +0300

    MovieExplorerApp-HomeBanner
    Anasayfada görüntülenen banner tamamlandı.

--- movieexplorerapp/src/components/HomeBanner/HomeBanner.jsx
+++ movieexplorerapp/src/components/HomeBanner/HomeBanner.jsx
@@ -1,11 +1,16 @@
+import "./homeBanner.scss";
 const HomeBanner = () => {
   return (
-    <section className="py-5">
+    <section className="py-5 homeBanner d-flex align-items-center text-center">
       <div className="container">
-        HomeBanner
+          <h1>Sinemanın Büyülü Dünyasında Keşfe Hazır Ol!</h1>
+          <p className="fs-2 mt-3 mb-0">
+            Film dünyasının sınırlarını zorlayan bir yolculuğa çıkın, her anı
+            büyüleyici keşiflerle dolu olsun!
+          </p>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
