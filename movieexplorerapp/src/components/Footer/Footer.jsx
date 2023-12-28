ee20ccbb248977c789d19ddb1e198cb39d456d38
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-12-28 19:28:24 +0300

    MovieExplorerApp-Footer-Update

---
+++ movieexplorerapp/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import FooterInfo from "./FooterInfo/FooterInfo";
+
+const Footer = () => {
+  return (
+    <footer className="py-5">
+      <div className="container">
+        <div className="row align-items-center">
+          <FooterInfo />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
