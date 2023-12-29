5d05edb67eea1bfea45771961962d4a76e8d5467
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-12-29 15:46:47 +0300

    MovieExplorerApp-Footer-Update

---
+++ movieexplorerapp/src/components/Footer/FooterBottom/FooterBottom.jsx
@@ -0,0 +1,31 @@
+const FooterBottom = () => {
+  return (
+    <div className="border-top py-4 mt-4 text-center">
+      <p className="mb-0">
+        Bu proje
+        <a
+          href="https://github.com/ahmetkanbaz"
+          target="_blank"
+          rel="noreferrer"
+          className="mx-1"
+        >
+          Ahmet KANBAZ
+        </a>
+        tarafından geliştirilmiştir.
+      </p>
+      <p className="mb-0">
+        Kullanılan API:
+        <a
+          href="https://www.omdbapi.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="ms-2"
+        >
+          OMDb API
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default FooterBottom;
